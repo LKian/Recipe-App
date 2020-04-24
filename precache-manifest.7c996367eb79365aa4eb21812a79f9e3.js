7eec6a8672af1367e9cd9e8afbad92b0c68fb998
Leah <LeahKahwadjian@gmail.com>
2020-04-24 15:24:51 -0400

    Updates

---
+++ precache-manifest.7c996367eb79365aa4eb21812a79f9e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21e7172927a3a5078d86e720ad0f282e",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "a5086ee9d6aae156b26e",
+    "url": "/recipe-app/static/css/main.e783a745.chunk.css"
+  },
+  {
+    "revision": "eb83b3c358fda1a4fe99",
+    "url": "/recipe-app/static/js/2.59145288.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-app/static/js/2.59145288.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5086ee9d6aae156b26e",
+    "url": "/recipe-app/static/js/main.5e2e89f7.chunk.js"
+  },
+  {
+    "revision": "9ecec974397cc002d6d3",
+    "url": "/recipe-app/static/js/runtime-main.85247353.js"
+  }
+]);
